5f7810ce69b0ee4bc2e84951bf037da3f5c58f58
skrupnyk <serhii.kr@upplabs.com>
2021-10-20 13:12:33 +0300

    refactoring (add variable)

--- js/build.js
+++ js/build.js
@@ -2,7 +2,9 @@ window.ui = window.ui || {};
 Fliplet.Widget.instance('list-thumb-l', function(data) {
   var $container = $(this);
 
-  $container.translate({ swipeToSaveLabel: data.swipeToSaveLabel || T('widgets.list.largeThumbs.defaultListName') });
+  var swipeToSaveLabel = data.swipeToSaveLabel || T('widgets.list.largeThumbs.defaultListName');
+
+  $container.translate({ swipeToSaveLabel: swipeToSaveLabel });
 
   function authenticateImages() {
     _.forEach(data.items, function(item) {
